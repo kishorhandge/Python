e9cec54146121f3ab0ef7280ad6374b93bbc893e
kishorhandge <kishorhandge29@gmail.com>
2026-01-05 23:40:11 +0530

    Add files via upload

---
+++ revisionall.py
@@ -0,0 +1,14 @@
+print("-"*50)
+
+arr = {10,20,30,40,10}
+print(arr)
+print(type(arr))
+
+print("-"*50)
+
+book = {"Name":"Let Us C","Price":350,"Author":"Y Kanetkar"}
+print(book)
+print(type(book))
+print(book["Price"])
+
+print("-"*50)
---
+++ revisionsequence.py
@@ -0,0 +1,11 @@
+ 
+print("-"*50)
+arr = [10,20,30,40,10]
+print(arr)
+print(type(arr))
+print("-"*50)
+
+brr = (10,20,30,40,10)
+print(brr)
+print(type(brr))
+print("-"*50)
