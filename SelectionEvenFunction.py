414143fc5b3d5492ad7ef77ff705c531f29e3210
kishorhandge <kishorhandge29@gmail.com>
2026-01-10 23:30:31 +0530

    Add files via upload

---
+++ SelectionEvenFunction.py
@@ -0,0 +1,14 @@
+def CheckEven(No):
+    if(No % 2 == 0):
+        print("It is Even")
+
+    else:
+        print("It is Odd")
+
+def main():
+    CheckEven(21)       # Positional
+    CheckEven(No = 22)  # keyword
+
+if __name__ == "__main__":
+    main()
+
