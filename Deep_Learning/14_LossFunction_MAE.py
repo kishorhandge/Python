6f13cbead8d4d44f5e1a70579c6125eb04c4dc72
kishorhandge <kishorhandge29@gmail.com>
2026-04-24 23:46:15 +0530

    Add files via upload

---
+++ Deep_Learning/14_LossFunction_MAE.py
@@ -0,0 +1,20 @@
+# Kishor MAE Loss Demonstration
+
+def Kishor_MAE(y_true, y_pred):
+    n = len(y_true)
+    total_error = 0
+
+    for i in range(n):
+        error = abs(y_true[i] - y_pred[i])
+        total_error += error
+
+    mae = total_error / n
+    return mae
+
+
+# Sample Data
+y_true = [10, 20, 30]
+y_pred = [12, 18, 33]
+
+loss = Kishor_MAE(y_true, y_pred)
+print("MAE Loss:", loss)
